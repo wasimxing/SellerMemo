9522d9ee2aacd9ae0661a0eb3152311fa4f82dfa
wasim <wasim@192.168.192.1>
2023-04-09 15:35:02 +0530

    first commit

---
+++ src/test/java/Practice/Groom.java
@@ -0,0 +1,15 @@
+package Practice;
+
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class Groom {
+	
+	@Parameters("BROWSER")
+	
+	@Test
+	public void m1(String browser) {
+		System.out.println(browser);
+	}
+
+}
